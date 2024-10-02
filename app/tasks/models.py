f6822266c7a31786b6cfd84db88b903caf5b3a1e
danik <daniilkashitsyn30@gmail.com>
2024-10-02 21:32:35 +0300

    ver 0.2

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -6,18 +6,18 @@ class Tasks(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    description = Column(String)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
     # group = Column(String)
     start_date = Column(Date)
     end_date = Column(Date)
-    priority = Column(JSON)
-    status = Column(JSON)
+    priority = Column(JSON, nullable=False)
+    status = Column(JSON, nullable=False)
 
 
 class Groups(Base):
     __tablename__ = "groups"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     # tasks = Column(String)
