58b59faf49563eedf2ff583f14c9513da6322b4b
danik <daniilkashitsyn30@gmail.com>
2024-11-11 14:01:57 +0300

    ver 0.2

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 
-from app.exceptions import TaskNotFoundException
-from app.tasks.dao import TasksDAO
-from app.tasks.shemas import STasks
+from app.exceptions import TaskNotFoundException, GroupNotFoundException
+from app.tasks.dao import TasksDAO, GroupsDAO
+from app.tasks.shemas import STasks, STaskCreate
 from app.users.dependencies import get_current_user
 from app.users.users import Users
 
@@ -20,3 +20,32 @@ async def get_task(task_id: int, user: Users = Depends(get_current_user)) -> STa
     if not task:
         raise TaskNotFoundException
     return task
+
+
+@router.post("/add-task")
+async def add_task(task_data: STaskCreate, user: Users = Depends(get_current_user)):
+    if task_data.group_id:
+        group = await GroupsDAO.find_by_id_and_user(id=task_data.group_id, user_id=user.id)
+        if group is None:
+            raise GroupNotFoundException
+    await TasksDAO.create_task(
+        name=task_data.name,
+        description=task_data.description,
+        group_id=task_data.group_id,
+        start_date=task_data.start_date,
+        end_date=task_data.end_date,
+        priority=task_data.priority,
+        status=task_data.status,
+        user_id=user.id
+    )
+    return {"message": "Задача создана"}
+
+
+@router.patch("{task_id}/ch")
+async def change_task_by_id(task_data, user: Users = Depends(get_current_user)):
+    pass
+
+
+@router.delete("{task_id}/del")
+async def delete_task_by_id(task_id, user: Users = Depends(get_current_user)):
+    pass
