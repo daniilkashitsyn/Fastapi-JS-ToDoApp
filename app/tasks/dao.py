82391e046aa07eaf59ca3aa8fa755c5104c053ba
danik <daniilkashitsyn30@gmail.com>
2024-11-11 14:01:57 +0300

    Added a post request for tasks

--- app/tasks/dao.py
+++ app/tasks/dao.py
@@ -1,9 +1,30 @@
+from sqlalchemy import insert
+
 from app.dao.base import BaseDAO
+from app.database import async_session_maker
 from app.tasks.models import Tasks, Groups
 
 
 class TasksDAO(BaseDAO):
     model = Tasks
+
+    @classmethod
+    async def create_task(cls, name: str, description: str, group_id: int, start_date, end_date, priority: str,
+                          status: str, user_id: int):
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(
+                name=name,
+                description=description,
+                group_id=group_id,
+                start_date=start_date,
+                end_date=end_date,
+                priority=priority,
+                status=status,
+                user_id=user_id
+            )
+
+            await session.execute(query)
+            await session.commit()
 
     @classmethod
     async def change_group(cls, group_name):
@@ -12,4 +33,3 @@ class TasksDAO(BaseDAO):
 
 class GroupsDAO(BaseDAO):
     model = Groups
-
--- app/tasks/shemas.py
+++ app/tasks/shemas.py
@@ -1,17 +1,31 @@
 from datetime import date
 
 from pydantic import BaseModel
+from typing import Optional
 
 
 class STasks(BaseModel):
     id: int
     name: str
-    description: str
-    group_id: int
-    start_date: date
-    end_date: date
-    priority: str
-    status: str
+    description: Optional[str] = None
+    group_id: Optional[int] = None
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
+    priority: Optional[str] = None
+    status: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+
+class STaskCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
+    group_id: Optional[int] = None
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
+    priority: Optional[str] = None
+    status: Optional[str] = None
 
     class Config:
         from_attributes = True
