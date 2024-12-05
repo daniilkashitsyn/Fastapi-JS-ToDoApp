85ba60875e6a8d8c0709f661ae647ec5c40be69f
danik <daniilkashitsyn30@gmail.com>
2024-12-05 13:16:32 +0300

    Revert "Added .idea files from templates"
    This reverts commit 2831245730be9718ba3b5626145810ec1ec44d79.

--- app/pages/router.py
+++ app/pages/router.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
 from app.tasks.tasks import get_tasks
-from app.tasks.groups import get_groups
 
 router = APIRouter(
     prefix="/pages",
@@ -33,20 +32,4 @@ async def reg_page(request: Request):
     return templates.TemplateResponse(
         name="auth/registration.html",
         context={"request": request}
-    )
-
-
-@router.get("/main")
-async def main_page(request: Request):
-    return templates.TemplateResponse(
-        name="main.html",
-        context={"request": request}
-    )
-
-
-@router.get("/groups")
-async def main_page(request: Request, groups=Depends(get_groups)):
-    return templates.TemplateResponse(
-        name="group.html",
-        context={"request": request, "groups": groups}
     )
