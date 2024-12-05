2831245730be9718ba3b5626145810ec1ec44d79
danik <daniilkashitsyn30@gmail.com>
2024-12-05 13:15:52 +0300

    Added .idea files from templates

--- app/pages/router.py
+++ app/pages/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
 from app.tasks.tasks import get_tasks
+from app.tasks.groups import get_groups
 
 router = APIRouter(
     prefix="/pages",
@@ -32,4 +33,20 @@ async def reg_page(request: Request):
     return templates.TemplateResponse(
         name="auth/registration.html",
         context={"request": request}
+    )
+
+
+@router.get("/main")
+async def main_page(request: Request):
+    return templates.TemplateResponse(
+        name="main.html",
+        context={"request": request}
+    )
+
+
+@router.get("/groups")
+async def main_page(request: Request, groups=Depends(get_groups)):
+    return templates.TemplateResponse(
+        name="group.html",
+        context={"request": request, "groups": groups}
     )
